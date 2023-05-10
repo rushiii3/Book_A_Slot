406085064f58824531e93e75c09b8655464e9464
hrushii <hrushiop@gmail.com>
2023-05-10 19:59:21 +0530

    signup_degin_done

--- src/main/sign_up.php
+++ src/main/sign_up.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body class="bg-light">
@@ -54,16 +55,16 @@
             <form>
             <div class="mb-3">
                     <label for="full_name" class="form-label">Full Name</label>
-                    <input type="name" class="form-control" id="full_name" placeholder="e.g. abc def">
+                    <input type="name" class="form-control" id="full_name" placeholder="e.g. abc def" required>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g. abc@vazecollege.net">
-                    <div id="emailHelp" class="form-text">Valid Email</div>
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailVerify" placeholder="e.g. abc@vazecollege.net" required>
+                    <div id="emailVerify" class="form-text"></div>
                 </div>
                 <div class="mb-3">
                     <label for="SelectDepartment" class="form-label">Select Department</label>
-                    <select class="form-select">
+                    <select class="form-select" required>
                       <option selected>Select Department</option>
                       <option value="1">One</option>
                       <option value="2">Two</option>
@@ -71,27 +72,33 @@
                     </select>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    <div id="emailHelp" class="form-text">Valid Email</div>
+                    <label for="password" class="form-label">Password</label>
+                    <div class="input-group">
+                      <input type="password" class="form-control" id="password" aria-describedby="pass_verify" placeholder="Password" required>
+                      <span class="input-group-text pass_icon" id="basic-addon1"><i class="bi bi-eye-fill pass_open_eye"></i> <i class="bi bi-eye-slash-fill pass_close_eye"></i></span>
+                    </div>
+                    <div id="pass_verify" class="form-text"></div>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirm Password">
-                    <div id="emailHelp" class="form-text">Valid Email</div>
+                    <label for="confirm_password" class="form-label">Confirm Password</label>
+                    <div class="input-group">
+                      <input type="password" class="form-control" id="confirm_password" aria-describedby="confirm_password_verify" placeholder="Confirm Password" required>
+                      <span class="input-group-text confirm_pass_icon" id="basic-addon1"><i class="bi bi-eye-fill cpass_open_eye"></i> <i class="bi bi-eye-slash-fill cpass_close_eye"></i></span>
+                    </div>
+                    <div id="confirm_password_verify" class="form-text"></div>
                 </div>
                 
-                <button type="submit" class="btn btn-primary px-5 py-2 ms-5 mt-3">Submit</button>
+                <button type="submit" id="submit" class="btn btn-primary px-5 py-2 ms-5 mt-3">Submit</button>
                 </form>
             </div>
             <div class="p-4 col-lg-6">
-                <img src="https://img.freepik.com/premium-vector/online-registration-sign-up-with-man-sitting-near-smartphone_268404-95.jpg?w=1480" alt="" class="img-fluid img-thumbnail">
+                <img src="https://img.freepik.com/premium-vector/online-registration-sign-up-with-man-sitting-near-smartphone_268404-95.jpg?w=1480" alt="" class="img-fluid">
                 <p class="text-center mt-5">
                     <a href="#" class="link-dark">Already have a account? sign in </a>
                 </p>
             </div>
         </div>
     </div>
-    <script src="/js/signup.js"></script>
+    <script src="../js/signup.js"></script>
 </body>
 </html>
