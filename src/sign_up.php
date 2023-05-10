720b577b31a930bbd48d0ff8f2bc22a7d92375aa
hrushii <hrushiop@gmail.com>
2023-05-10 18:03:12 +0530

    dd

--- src/sign_up.php
+++ src/sign_up.php
@@ -7,8 +7,9 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
-<body>
+<body class="bg-light">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -44,34 +45,53 @@
 
 
 
-    <div class="container w-75 bg-light mt-5 rounded">
+    <div class="container w-75 mt-5 mb-5 shadow p-3 mb-5 bg-body" style="border-radius:20px;">
         <div class="row p-3">
             <div class="col-lg-12">
                 <p class="h1">Sign up</p>
             </div>
             <div class="p-1 col-lg-6">
             <form>
+            <div class="mb-3">
+                    <label for="full_name" class="form-label">Full Name</label>
+                    <input type="name" class="form-control" id="full_name" placeholder="e.g. abc def">
+                </div>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g. abc@vazecollege.net">
                     <div id="emailHelp" class="form-text">Valid Email</div>
                 </div>
                 <div class="mb-3">
+                    <label for="SelectDepartment" class="form-label">Select Department</label>
+                    <select class="form-select">
+                      <option selected>Select Department</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                </div>
+                <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <div id="emailHelp" class="form-text">Valid Email</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword2">
+                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirm Password">
+                    <div id="emailHelp" class="form-text">Valid Email</div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                
+                <button type="submit" class="btn btn-primary px-5 py-2 ms-5 mt-3">Submit</button>
                 </form>
             </div>
             <div class="p-4 col-lg-6">
-                <img src="/img/register.png" alt="" class="img-thumbnail">
-                <a href="#">Already have a account? sign in </a>
+                <img src="https://img.freepik.com/premium-vector/online-registration-sign-up-with-man-sitting-near-smartphone_268404-95.jpg?w=1480" alt="" class="img-fluid img-thumbnail">
+                <p class="text-center mt-5">
+                    <a href="#" class="link-dark">Already have a account? sign in </a>
+                </p>
             </div>
         </div>
     </div>
+    <script src="/js/signup.js"></script>
 </body>
 </html>
