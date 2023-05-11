7c741425cdffbecf885d8472e329932d06e110e1
hrushii <hrushiop@gmail.com>
2023-05-11 15:28:06 +0530

    connection

---
+++ src/main/connection/connect.php
@@ -0,0 +1,10 @@
+<?php
+$con = mysqli_connect("localhost","anushka","!RqkW_eW-td3y3km","Book_A_Slot");
+if(mysqli_connect_errno()){
+    echo("not connected");
+}else{
+    echo("connected");
+}
+
+
+?>
