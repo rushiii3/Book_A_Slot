03186b3fef643ed7e1ff5f4f73ac8521c7187470
hrushii <hrushiop@gmail.com>
2023-05-15 23:31:39 +0530

    update

--- src/main/booking.php
+++ src/main/booking.php
@@ -278,15 +278,3 @@
     <script src="../js/bookingDate.js"></script>
 </body>
 </html>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-    flatpickr("#myDatePicker", {
-      dateFormat: 'Y-m-d',
-      minDate: new Date().fp_incr(3),
-      disable: [
-        '2023-05-20',
-        '2023-05-25'
-      ]
-    });
-  });
-</script>
