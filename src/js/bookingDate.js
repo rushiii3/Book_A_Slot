7c6c46c39a3b86173e068701c2d04b9ff5180e77
hrushii <hrushiop@gmail.com>
2023-05-16 18:29:37 +0530

    senddd

--- src/js/bookingDate.js
+++ src/js/bookingDate.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    flatpickr("#myDatePicker", {
+    flatpickr("#selectDate", {
       dateFormat: 'Y-m-d',
       minDate: new Date().fp_incr(3),
       disable: [
@@ -8,3 +8,29 @@ document.addEventListener('DOMContentLoaded', function() {
       ]
     });
   });
+
+ 
+$start_time = $('.start-time');
+$end_time = $('.end-time');
+var start_timee = $start_time[2].innerHTML;
+var end_timee = $start_time[6].innerHTML;
+function BlockTime(start_timee,end_timee)
+{
+    
+    var i,x = $('.start-time');
+    //console.log(x.length);
+    for(i=0;i<x.length;i++)
+    {
+        if(start_timee==$start_time[i].innerHTML)
+        { //$start_time.eq(i).prop('disabled', true);
+            do{
+                $start_time.eq(i).prop('disabled', true);
+                $end_time.eq(i).prop('disabled', true);
+                //console.log(i);
+                i=i+1;
+            }while(end_timee>=$start_time[i].innerHTML);
+        }
+    }
+}
+BlockTime(start_timee,end_timee)
+
