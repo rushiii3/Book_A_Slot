3cf029ede03b7a62e4a5855a6567af96315109db
hrushii <hrushiop@gmail.com>
2023-05-20 16:18:48 +0530

    commit

--- src/js/booking.js
+++ src/js/booking.js
@@ -1,91 +1,37 @@
-$('.tab').hide();
-var currentTab = 0;
-showTab(currentTab)
-function showTab(n)
-{
-    $x = $('.tab');
-    $x.eq(n).show();
-    if (n === 0) {
-    $('#nextBtn').addClass('first_page');
-    $("#prevBtn").hide();
-  } else {
-    $('#nextBtn').removeClass('first_page');
-    $('#nextBtn').removeClass('second_page');
-    $("#prevBtn").show();
-  }
-
-  if(n === 1){
-    $('#nextBtn').addClass('second_page');
-    $('#nextBtn').removeClass('first_page');
-  }
-
-  if (n === 2) {
-    $("#nextBtn").html("Submit");
-    //$('#nextBtn').attr('onclick', 'submitValue()');
-    //$('#nextBtn').attr('type', 'submit');
-  } else {
-    $("#nextBtn").html("Next");
-    //$('#nextBtn').attr('onclick', 'nextPrev(1)');
-    //$('#nextBtn').attr('type', 'button');
-    //$("#nextBtn").removeClass("submit")
-  }
-  CurrentStep(n);
-}
-function nextPrev(n)
-{
+$('#org_institue_name').hide();
+$('#department_namee').on('change',function(){
     
-    $x = $(".tab");
-    $x.eq(currentTab).hide();
+    $department_namee = $('#department_namee').val();
     
-    currentTab = currentTab + n;
-    if(currentTab<=2)
+    if($department_namee==="Others")
     {
-        //console.log("yes"); 
+        console.log("others selected");
+        $('#org_institue_name').show();
+        $('#Institute_OrgName').val("");
     }
     else{
-        currentTab=2;
-        //console.log("no");
+        $('#org_institue_name').hide();
+        $('#Institute_OrgName').val($department_namee);
     }
-    showTab(currentTab);
     
-}
-function CurrentStep(n)
-{
-    var i, x = document.getElementsByClassName("stepper-item");
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace("active", "");
-  }
-  //... and adds the "active" class to the current step:
-  x[n].className += " active";
-}
-/*
-function Completed(n,currentTab)
-{
-    var i, x = document.getElementsByClassName("stepper-item");
-    console.log(currentTab);
-    if(n==1){
-        if(currentTab<2)
-    {
-        console.log("inside 1");
-        for(i=0;i<=currentTab;i++)
-        {
-            $step = $('.stepper-item');
-            $step.eq(i).addClass("completed");
-        } 
-    }
-        
-    }
-    else{
-        console.log("inside -1");
-        console.log(currentTab);
-            $step = $('.stepper-item');
-            $step.eq(currentTab-1).removeClass("completed");
-        }
-}
-*/
- 
- 
-$('.first_page').on('click',function()
+}) 
+$('.section2').hide();
+$('.section3').hide();
+$step = $('.stepper-item');
+$step.eq(0).addClass("active");
+$('#prevBtnSecond').on('click',function(){
+    $('.section2').hide();
+    $('.section1').show();
+    $step.eq(0).addClass("active");
+    $step.eq(1).removeClass("active");
+})
+$('#prevBtnThird').on('click',function(){
+    $('.section3').hide();
+    $('.section2').show();
+    $step.eq(1).addClass("active");
+    $step.eq(2).removeClass("active");
+})
+$('#nextFirst').on('click',function()
 {
     $event_name = $('#eventName').val();
     $event_Descr = $('#eventDescription').val();
@@ -105,9 +51,12 @@ $('.first_page').on('click',function()
             {
               if($event_end_time!=="Select the end time")
               {
-                nextPrev(1);
-                $step = $('.stepper-item');
+                $step.eq(0).removeClass("active");
                 $step.eq(0).addClass("completed");
+                $('.section1').hide();
+                $('.section2').show();
+                $step.eq(1).addClass("active");
+                
               }else{
                 alert("Please input the event end time");
               }
@@ -134,9 +83,126 @@ $('.first_page').on('click',function()
       alert("Please input your Event Name");
     }
 })
-$('.second_page').on('click',function()
-{
 
-  console.log("no forward");
+
+$('#nextSecond').on('click',function()
+{
+    $department_namee = $('#department_namee').val();
+    $Institute_OrgName = $('#Institute_OrgName').val();
+    $Venue_name = $('#Venue_name').val();
+    if($department_namee!=="Select Department")
+    {
+        if($Institute_OrgName!==""){
+            if($Venue_name!=="Select Venue"){
+                $step.eq(1).removeClass("active");
+                $step.eq(1).addClass("completed");
+                $('.section1').hide();
+                $('.section2').hide();
+                $('.section3').show();
+                $step.eq(2).addClass("active");
+            }else{
+                alert("Please select your venue");
+            }
+        }
+        else{
+            alert("Please Input your Institute/Organisation Name");
+        }
+    }else{
+        alert("Please select department first");
+    }
 
 })
+
+$('#nextThird').on('click',function(e){
+    $rp_name = $('#rp_name').val();
+    $companyName = $('#companyName').val();
+    $designation = $('#designation').val();
+    $experience = $('#experience').val();
+    if($rp_name !== ""){
+        if($companyName!==""){
+            if($designation!==""){
+                if($experience!==""){
+                  if($('#terms_condition').is(':checked'))
+                  {
+                      
+                    $event_name = $('#eventName').val();
+                  $event_Descr = $('#eventDescription').val();
+                  $num_of_students = $('#no_of_stu_attending').val();
+                  $event_date = $('#selectDate').val();
+                  $event_start_time = $('#selectStartTime').val();
+                  $event_end_time = $('#selectEndTime').val();
+                  $Institute_OrgName = $('#Institute_OrgName').val();
+                  $Venue_name = $('#Venue_name').val();
+                  $rp_name = $('#rp_name').val();
+                  $companyName = $('#companyName').val();
+                  $designation = $('#designation').val();
+                  $experience = $('#experience').val();
+                  $user_email = $('#user_email').val();
+                  if(
+                    $user_email!==""
+                        &&
+                    $event_name!==""
+                        &&
+                    $event_Descr!==""
+                        && 
+                    $num_of_students!==""
+                        &&
+                    $event_date!==""
+                        &&
+                    $event_start_time!=="Select the start time"
+                        &&
+                    $event_end_time!=="Select the end time"
+                        &&
+                    $Institute_OrgName!==""
+                        &&
+                    $Venue_name!=="Select Venue"
+                        &&
+                    $rp_name!==""
+                        &&
+                    $companyName!==""
+                        &&
+                    $designation!==""
+                        &&
+                    $experience!==""
+              
+                  ){
+                    $.ajax({
+                      type: 'POST',
+                      url: 'ajax.php',
+                      data: {user_email:$user_email,eventname : $event_name, event_desc: $event_Descr, no_of_students : $num_of_students, event_date : $event_date, event_start_time:$event_start_time, event_end_time:$event_end_time, Institute_OrgName:$Institute_OrgName, Venue_name:$Venue_name, rp_name:$rp_name, companyName:$companyName, designation:$designation, experience:$experience  },
+                      success: function(data){
+                          console.log(data);
+                          if(data==1)
+                          {
+                            $('#success').modal('show');
+                          }
+                          else
+                          {
+                            $('#failed').modal('show');
+                          }
+                      },
+                      error: function() {
+                          console.log(response.status);
+                      },
+                  })
+                  }
+
+                  }else{
+                     alert("Please agree our terms and condition");
+                  }
+                  }else{
+                    alert("Please agree the Terms and Condition");
+                  }  
+            }else{
+                alert("Please input the resource person designation");
+            }
+        }else{
+            alert("Please input the resource person company name");
+        }
+    }else{
+        alert("Please input the resource person name");
+    }
+    e.preventDefault();
+})
+
+
