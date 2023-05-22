c2f14ccafc560229ec7ed749919cda0ad5eda735
123nush <anushkadal1611@gmail.com>
2023-05-22 17:11:05 +0530

    month report added

--- src/main/update_dept.php
+++ src/main/update_dept.php
@@ -13,30 +13,27 @@ include './connection/connect.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <style>
-        /* .center-table {
-        margin-left: auto;
-        margin-right: auto;
-        text-align: center;
-    }
-    .con {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    } */
+       
     .con1{
         display: grid;
         place-items: center;
     }
         </style>
 </head>
+<?php
+if(isset($_POST['no'])){
+    header('location:./admin_home.php?update_dept');
+}
+?>
 <body class=' text-center w-50 m-auto'>
     <div class="con1 mt-3 ">
     <h1 class='text-center text-primary'>Update Department</h1>
     <table  class='table table-bordered my-4' style="align-items:center">
-    <thread class="bg-info">
+    <thread class="bg-light">
         <tr >
         <th class="text-center"> Sr. No</th>
            <th class="text-center"> DEPARTMENT NAMES</th>
+          
         </tr>
         </thread> 
     <tbody class="bg-primary">
@@ -49,11 +46,82 @@ include './connection/connect.php';
             $number++;
             echo"
             <tr class='text-center text-light'><td> $number</td>
-            <td><a class='text-light' style='text-decoration:none' href='update.php?update_id=$department_name'> $department_name</td></tr>";
+            <td><a class='text-light' style='text-decoration:none' data_id='$department_name' onclick='confirmupdate(this)'
+            type='button' class='text-light' data-toggle='modal' 
+            data-target='#update_dept'> $department_name</td></tr>";
+            // href='update.php?update_id=$department_name'
             }
             ?>
         </tbody>    
     </table>
     </div>
+    <?php
+    // to update data
+    if(isset($_POST['update_dept'])){
+  $department_name=$_POST['department_name_original'];
+    $dept=$_POST['department_name_new'];
+    if($dept==''){
+      $sql="UPDATE `DEPARTMENT` set department_name='$department_name' where department_name='$department_name' ";
+      $result=mysqli_query($con,$sql);
+      if($result){
+        //echo "<p class='text-aign'>Data updated Successfully ";
+        header("location:./admin_home.php?update_dept");
+      }
+      else{
+        echo "<p class='text-aign'>Data Is Not updated Successfully ";
+        header("location:./admin_home.php?update_dept");
+      }
+
+    }
+    else{
+    $sql="UPDATE `DEPARTMENT` set department_name='$dept' where department_name='$department_name' ";
+      $result=mysqli_query($con,$sql);
+      if($result){
+        //echo "<p class='text-aign'>Data updated Successfully ";
+        header("location:./admin_home.php?update_dept");
+      }
+      else{
+        echo "<p class='text-aign'>Data Is Not updated Successfully ";
+        header("location:./admin_home.php?update_dept");
+      }}
+}
+?>
+<script>
+  function confirmupdate(self){
+    var department_name_original=self.getAttribute("data_id");
+    document.getElementById("form_data").department_name_original.value=department_name_original;
+    $('#update_dept').modal('show');
+  }
+</script>
+     <!-- Modal -->
+<div class="modal fade" id="update_dept" tabindex="-1" role="dialog" aria-labelledby="update_dept" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+    <h2 class="text-center">Update Department Name</h2>
+    <form action="update_dept.php" method="POST" id="form_data">
+      <div class="modal-body">
+      <div class="form-outline w-100 m-auto">
+                    <label for="department_name" id="department_name" class="form-label fw-bold">Old Department Name</label>
+                    <input type="text" id="department_name_original" name="department_name_original" readonly class="form-control" placeholder="Enter a depertment name">
+                </div> 
+                <div class="form-outline w-100 m-auto">
+                    <label for="department_name" id="department_name" class="form-label fw-bold">Department Name</label>
+                    <input type="text" id="department_name_new" name="department_name_new" class="form-control" placeholder="Enter a depertment name">
+                </div>             
+      </div>
+    <div class="modal-footer">
+        <button type="submit" class="btn btn-secondary" name='no'>close</button>
+        <button type="submit" class="btn btn-primary" name='update_dept'>
+        UPDATE</button>
+    </div>
+    </form> 
+    
+    </div>
+  </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js" integrity="sha512-5SUkiwmm+0AiJEaCiS5nu/ZKPodeuInbQ7CiSrSnUHe11dJpQ8o4J1DU/rw4gxk/O+WBpGYAZbb8e17CDEoESw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+
 </body>
 </html>
