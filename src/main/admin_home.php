e9c444c1ee470666f4a150569db1619426666586
123nush <anushkadal1611@gmail.com>
2023-05-23 16:10:33 +0530

    report home

--- src/main/admin_home.php
+++ src/main/admin_home.php
@@ -81,6 +81,7 @@ include './connection/connect.php';
                                 <li class="nav-item">
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                                 <a class="nav-link  my-2 " href="report_home.php" tabindex="-1" aria-disabled="true">REPORTS</a>
 =======
                                 <a class="nav-link  my-2 " href="#" tabindex="-1" aria-disabled="true">REPORTS</a>
@@ -88,6 +89,9 @@ include './connection/connect.php';
 =======
                                 <a class="nav-link  my-2 " href="#" tabindex="-1" aria-disabled="true">REPORTS</a>
 >>>>>>> 7f010e3 (admin)
+=======
+                                <a class="nav-link  my-2 " href="report_home.php" tabindex="-1" aria-disabled="true">REPORTS</a>
+>>>>>>> 728e3da (report home)
                                 </li>
                                 
                             </ul>
